ed78c9e2151233d9bfc77b95034c808acc2c682f
Claudio <me@p4p3r.dev>
2023-06-13 10:54:11 +0200

    Update search.ts

--- routes/search.ts
+++ routes/search.ts
@@ -22,8 +22,7 @@ module.exports = function searchProducts () {
     criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200)
     models.sequelize.query(`SELECT * FROM Products
                             WHERE ((name LIKE '%${criteria}%' OR description LIKE '%${criteria}%')
-                                    AND deletedAt IS NULL)
-                            ORDER BY name`) // vuln-code-snippet vuln-line unionSqlInjectionChallenge dbSchemaChallenge
+                                    AND deletedAt IS NULL)`) // vuln-code-snippet vuln-line unionSqlInjectionChallenge dbSchemaChallenge
       .then(([products]: any) => {
         const dataString = JSON.stringify(products)
         if (challengeUtils.notSolved(challenges.unionSqlInjectionChallenge)) { // vuln-code-snippet hide-start
